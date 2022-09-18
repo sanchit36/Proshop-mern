deaea9b2c05aa2dd3935e14ab36f56f21039dfe1
Sanchit Bhadgal <sanchitbhadgal36@gmail.com>
2022-09-18 14:57:39 +0530

    user registration api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,37 @@ export const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    throw new AppError('User already exists', 400);
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    throw new AppError('Invalid user data', 400);
+  }
+});
+
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
