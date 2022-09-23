40515be57aca4eb3a7f181be65920a593b381fd8
Sanchit Bhadgal <sanchitbhadgal36@gmail.com>
2022-09-23 17:23:18 +0530

    added product create and update route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -33,3 +33,45 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     throw new AppError('Product not found', 404);
   }
 });
+
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = await Product.create({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+  res.status(201).json(product);
+});
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    throw new AppError('Product not found', 404);
+  }
+});
